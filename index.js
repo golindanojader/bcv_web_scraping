c5f4ba228ac2e014d476aac41f13b9634e8028ba
Jader Golindano <golindano.jader@gmail.com>
2024-12-10 14:35:43 -0400

    rutina proceso automatico cron

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 
 // npm install @extractus/article-extractor
 
-
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import {extract} from '@extractus/article-extractor'
 import fs from 'fs';
+import CronJob from 'node-cron';
 let ruta = 'tasa_bcv.txt'
 let texto = "Esto es una prueba \n\t";
-
-
 
 const fechaActual = new Date();
 
@@ -24,12 +21,24 @@ const segundos = fechaActual.getSeconds();
 // Formatear la fecha y hora
 const fechaFormateada = `${año}-${mes}-${día} ${horas}:${minutos}:${segundos}`;
 
-
 let web = await extract('https://www.bcv.org.ve/')
 const titulo = "tasa BCV: "
 var output = JSON.stringify(web)
-console.log(titulo + output.slice(1783,1789))
 
-fs.writeFile(ruta, '', function (err){})
-fs.appendFile(ruta, output.slice(1783,1789), function (err) {if (err) throw err;});
-  
+// var CronJob = require('cron').CronJob;
+
+
+ CronJob.schedule('* * * * *', ()=> {
+    
+    console.log(titulo + output.slice(1783,1789))
+
+    fs.writeFile(ruta, '', function (err){})
+    fs.appendFile(ruta, output.slice(1783,1789), function (err) {if (err) throw err;});
+    
+
+}
+
+)
+
+
+
