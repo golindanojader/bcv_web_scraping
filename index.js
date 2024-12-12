c33b6d7d24f0df2f4128493f6893bdbc4b3786b6
Jader Golindano <golindano.jader@gmail.com>
2024-12-12 09:32:40 -0400

    Modificacion, actualizacion correcta tasa

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-
 // npm install @extractus/article-extractor
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 import {extract} from '@extractus/article-extractor'
 import fs from 'fs';
 import CronJob from 'node-cron';
+
 let ruta = 'tasa_bcv.txt'
-let texto = "Esto es una prueba \n\t";
-
-const fechaActual = new Date();
-
-// Obtener componentes individuales
-const año      = fechaActual.getFullYear();
-const mes      = fechaActual.getMonth() + 1; 
-const día      = fechaActual.getDate();
-const horas    = fechaActual.getHours();
-const minutos  = fechaActual.getMinutes();
-const segundos = fechaActual.getSeconds();
-
-// Formatear la fecha y hora
-const fechaFormateada = `${año}-${mes}-${día} ${horas}:${minutos}:${segundos}`;
-
-let web = await extract('https://www.bcv.org.ve/')
-const titulo = "tasa BCV: "
-var output = JSON.stringify(web)
-
-// var CronJob = require('cron').CronJob;
 
 
- CronJob.schedule('* * * * *', ()=> {
-    
-    console.log(titulo + output.slice(1783,1789))
 
-    fs.writeFile(ruta, '', function (err){})
-    fs.appendFile(ruta, output.slice(1783,1789), function (err) {if (err) throw err;});
-    
 
-}
+ CronJob.schedule('*/59 * * * *', async()=> {
+	 
+	 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-)
+	   var fechaActual = new Date();
+		// Obtener componentes individuales
+		var año      = fechaActual.getFullYear();
+		var mes      = fechaActual.getMonth() + 1; 
+		var día      = fechaActual.getDate();
+		var horas    = fechaActual.getHours();
+		var minutos  = fechaActual.getMinutes();
+		var segundos = fechaActual.getSeconds();
 
+		// Formatear la fecha y hora
+		var fechaFormateada = `${año}-${mes}-${día} ${horas}:${minutos}:${segundos}`;
+
+		
+		var web = await extract('https://www.bcv.org.ve/')
+		const titulo = "tasa BCV: "
+		var output = JSON.stringify(web)
+				
+		
+		console.log(fechaFormateada + '-' + titulo + output.slice(1783,1789))
+
+		fs.writeFile(ruta, '', function (err){})
+		fs.appendFile(ruta, output.slice(1783,1789), function (err) {if (err) throw err;});
+	   
+
+})
 
 
