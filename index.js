1e1d518576cc839d4276ba72e62c8f1379e009b0
Jader Golindano <golindano.jader@gmail.com>
2024-12-13 11:28:58 -0400

    Adecuacion redondeo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// npm install @extractus/article-extractor
-
+// npm install @extractus/article-extractor 
+//DESARROLLO FINAL  13-12-2024 POR JADER GOLINDANO
 
 import {extract} from '@extractus/article-extractor'
 import fs from 'fs';
@@ -7,14 +7,11 @@ import CronJob from 'node-cron';
 
 let ruta = 'tasa_bcv.txt'
 
-
-
-
  CronJob.schedule('*/59 * * * *', async()=> {
 	 
-	 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+	    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-	   var fechaActual = new Date();
+	    var fechaActual = new Date();
 		// Obtener componentes individuales
 		var año      = fechaActual.getFullYear();
 		var mes      = fechaActual.getMonth() + 1; 
@@ -25,19 +22,21 @@ let ruta = 'tasa_bcv.txt'
 
 		// Formatear la fecha y hora
 		var fechaFormateada = `${año}-${mes}-${día} ${horas}:${minutos}:${segundos}`;
-
-		
 		var web = await extract('https://www.bcv.org.ve/')
 		const titulo = "tasa BCV: "
 		var output = JSON.stringify(web)
+		var value_from_web   = output.slice(1783,1791)
+		var replace_value    = value_from_web.replace(',', '.')
+		var convert_to_float = parseFloat(replace_value)
+		var rounded_value    = convert_to_float.toFixed(2)
+		var final_value      = rounded_value.replace('.', ',')
 				
-		
-		console.log(fechaFormateada + '-' + titulo + output.slice(1783,1789))
+		console.log(fechaFormateada + '-' + titulo + final_value)
 
 		fs.writeFile(ruta, '', function (err){})
-		fs.appendFile(ruta, output.slice(1783,1789), function (err) {if (err) throw err;});
+	    fs.appendFile(ruta, final_value, function (err) {if (err) throw err;});
 	   
 
-})
 
 
+	})
